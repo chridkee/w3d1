645a43fea9ddac3a383b7429d9a372722fbce812
chridkee <christopher.keesor@gmil.com>
2023-01-23 17:32:27 -0500

    select method update

--- w3d1.rb
+++ w3d1.rb
@@ -1,1 +1,24 @@
-puts "Hello"
+class Array 
+    def my_each(&prc)
+        i = 0 
+        while i < self.length 
+            prc.call(self[i])
+            i+=1
+        end
+    end
+
+    def my_select(&prc)
+        arr = []
+        self.my_each do |ele|
+            if prc.call(ele) == true 
+                arr << ele
+            end
+        end
+        return arr 
+    end
+
+end
+
+print a = [1, 2, 3]
+print a.my_select { |num| num > 1 } # => [2, 3]
+print a.my_select { |num| num == 4 } # => []
